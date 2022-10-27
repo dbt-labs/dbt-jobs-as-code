422152d79db94e4c31a7b7713d66dd882fd60fad
George Sass <george.sass@dbtlabs.com>
2022-10-27 14:05:57 -0400

    Flesh out env var type

--- src/schemas/custom_environment_variable.py
+++ src/schemas/custom_environment_variable.py
@@ -1,4 +1,20 @@
 import pydantic
 
-class CustomEnvironmentVariable:
-    pass
+from typing import Literal, Optional
+
+class CustomEnvironmentVariable(pydantic.BaseModel):
+    account_id: int
+    project_id: int
+    name: str = 'DBT_VARIABLE'
+    type: Literal['project', 'environment', 'job', 'user'] = 'job'
+
+    value: Optional[str]
+
+    def validate(self):
+        if not self.value:
+            return "Value must be defined!"
+        if not self.name.startswith('DBT_'):
+            return "Key must have `DBT_` prefix."
+        if not self.name.isupper():
+            return "Key name must be SCREAMING_SNAKE_CASE"
+        return None
