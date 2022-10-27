2fddc9d45586d6f68d9ac27939826a96be23ea19
Nicholas Yager <nicholas.yager@dbtlabs.com>
2022-10-27 19:29:54 -0400

    Cleanup: Remove excess dbt Cloud Client code

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -4,6 +4,7 @@ import requests
 
 from schemas.job import JobDefinition
 
+
 class DBTCloud:
     """A minimalistic API client for fetching dbt Cloud data."""
 
@@ -20,97 +21,24 @@ class DBTCloud:
         if not self.account_id:
             raise Exception("An account_id is required to get dbt Cloud jobs.")
 
-    def get_latest_job_runs(self, job_id: int) -> Dict:
-        """Obtain the most recent run for a job"""
-        offset = 0
-
-        run = None
-
-        while True:
-            parameters = {
-                "offset": offset,
-                "job_definition_id": job_id,
-                "order_by": "-id",
-            }
-
-            response = requests.get(
-                url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/runs/",
-                params=parameters,
-                headers={
-                    "Authorization": f"Bearer {self._api_key}",
-                    "Content-Type": "application/json",
-                },
-            )
-
-            run_data = response.json()
-
-            for specficic_run in run_data["data"]:
-                if specficic_run["is_success"]:
-                    logger.debug(f"Using run {specficic_run['id']}")
-                    run = specficic_run
-                    break
-
-            if run is not None:
-                break
-
-            if (
-                run_data["extra"]["filters"]["limit"]
-                + run_data["extra"]["filters"]["offset"]
-                >= run_data["extra"]["pagination"]["total_count"]
-            ):
-                break
-
-            offset += run_data["extra"]["filters"]["limit"]
-
-        if run is None:
-            raise Exception("Unable to find a latest run.")
-
-        return run
-
-    def get_latest_manifest(self, environemnt_id: int) -> Dict:
-        """Return the most recently generated manifest.json file for an environment"""
-
-        # Find the most recent run for an environemnt.
-
-        self._check_for_creds()
-
-        jobs = self.get_jobs(environemnt_id)
-
-        if len(jobs) == 0:
-            raise Exception("No recent jobs found.")
-
-        run = self.get_latest_job_runs(jobs[0]["id"])
-
-        manifest_response = requests.get(
-            url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/runs/{run['id']}/artifacts/manifest.json",
-            headers={
-                "Authorization": f"Bearer {self._api_key}",
-                "Content-Type": "application/json",
-            },
-        )
-
-        return manifest_response.json()
-
     def create_job(self, job: JobDefinition) -> Dict:
         """Create a dbt Cloud Job using a JobDefinition"""
         payload = job.to_payload()
 
         response = requests.post(
-                url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/jobs/",
-                headers={
-                    "Authorization": f"Bearer {self._api_key}",
-                    "Content-Type": "application/json",
-                },
-                data=payload
-            )
+            url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/jobs/",
+            headers={
+                "Authorization": f"Bearer {self._api_key}",
+                "Content-Type": "application/json",
+            },
+            data=payload
+        )
 
         print(response.request.__dict__)
 
         print(response.json())
 
-
         return response.json()
-
 
     # TODO: Return a List[JobDefinition] instead!
     def get_jobs(self) -> List[Dict]:
@@ -141,9 +69,9 @@ class DBTCloud:
             jobs.extend(job_data["data"])
 
             if (
-                job_data["extra"]["filters"]["limit"]
-                + job_data["extra"]["filters"]["offset"]
-                >= job_data["extra"]["pagination"]["total_count"]
+                    job_data["extra"]["filters"]["limit"]
+                    + job_data["extra"]["filters"]["offset"]
+                    >= job_data["extra"]["pagination"]["total_count"]
             ):
                 break
 
