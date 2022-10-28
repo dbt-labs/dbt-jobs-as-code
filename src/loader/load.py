2af98f83ad988484e06dcff20f673d1bca1f33eb
Nicholas Yager <nicholas.yager@dbtlabs.com>
2022-10-27 20:46:11 -0400

    Feat: Refactor YAML loading into a function

--- src/loader/load.py
+++ src/loader/load.py
@@ -1,4 +1,16 @@
-from ..schemas.job import JobDefinition
+from typing import Dict
 
-if __name__ == '__main__':
-    pass
+import yaml
+
+from schemas.job import JobDefinition
+
+
+def load_job_definitions(path) -> Dict[str, JobDefinition]:
+    """Load a job YAML file into a dictionary of JobDefinitions"""
+    with open(path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+    return {
+        identifier: JobDefinition(**job, identifier=identifier)
+        for identifier, job in config['jobs'].items()
+    }
--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
 import os
-import yaml
 
-from schemas.job import JobDefinition
 from client import DBTCloud
+from loader.load import load_job_definitions
 
 if __name__ == '__main__':
     absolute_path = os.path.dirname(__file__)
     example_path = '../supporting-code/jobs.yml'
-    configs = open(
-        os.path.join(absolute_path, example_path),
-        'r'
+    path = os.path.join(absolute_path, example_path)
+
+    defined_jobs = load_job_definitions(path)
+
+    print(defined_jobs)
+
+    dbt_cloud = DBTCloud(
+        account_id=43791,
+        api_key=os.environ.get('API_KEY')
     )
-    pyconfigs = yaml.safe_load(configs)
-    pydantic_job_definition = JobDefinition(**pyconfigs['jobs'][0])
-    print(pydantic_job_definition)
+    cloud_jobs = dbt_cloud.get_jobs()
+    tracked_jobs = {job.identifier: job for job in cloud_jobs if job.identifier is not None }
 
-    dbt_cloud = DBTCloud(account_id=43791, api_key=os.environ.get('API_KEY'))
+    print(tracked_jobs)
 
-    response = dbt_cloud.create_job(pydantic_job_definition)
-    print(response)
-
-    response.generate_docs = True
-    updated_job = dbt_cloud.update_job(response)
-
-    print(updated_job)
+    #
+    #
+    # response = dbt_cloud.create_job(pydantic_job_definition)
+    # print(response)
+    #
+    # response.generate_docs = True
+    # updated_job = dbt_cloud.update_job(response)
+    #
+    # print(updated_job)
