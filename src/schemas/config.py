400bf4c937da29743cc30feb40f57493d66b8f23
Nicholas Yager <nicholas.yager@dbtlabs.com>
2022-10-28 16:09:39 -0400

    Add validator for matching job account_id

--- src/schemas/config.py
+++ src/schemas/config.py
@@ -1,6 +1,8 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 
 import pydantic
+from loguru import logger
+from pydantic import validator
 
 from schemas import JobDefinition
 
@@ -10,3 +12,22 @@ class Config(pydantic.BaseModel):
 
     account_id: int
     jobs: Dict[str, JobDefinition]
+
+    @validator('jobs')
+    def jobs_must_have_none_or_matching_account_id(cls, v, values, **kwargs):
+        """Confirm that each job has either a None account_id, or that the `account_id` matches self.account_id"""
+        for identifier, job in v.items():
+            if job.account_id != values['account_id']:
+                raise ValueError(
+                    f'The job `{identifier}` contains an `account_id` that does not match the global config account_id.'
+                )
+        return v
+
+    def __init__(self, **data: Any):
+
+        # Check for instances where account_id is missing from a job, and add it from the config data.
+        for identifier, job in data.get('jobs', dict()).items():
+            if 'account_id' not in job or job['account_id'] is None:
+                job['account_id'] = data['account_id']
+
+        super().__init__(**data)
