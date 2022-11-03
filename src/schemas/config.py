502fab23f0391c5b49dfd977752a4a6f92448eca
Nicholas Yager <nicholas.yager@dbtlabs.com>
2022-11-03 13:03:34 -0400

    Remove validation and remove top-level `account_id` field

--- src/schemas/config.py
+++ src/schemas/config.py
@@ -1,7 +1,6 @@
 from typing import Dict, Any
 
 import pydantic
-from pydantic import validator
 
 from schemas import JobDefinition
 
@@ -9,18 +8,7 @@ from schemas import JobDefinition
 class Config(pydantic.BaseModel):
     """Internal representation of a Jobs as Code configuration file."""
 
-    account_id: int
     jobs: Dict[str, JobDefinition]
-
-    @validator("jobs")
-    def jobs_must_have_none_or_matching_account_id(cls, v, values, **kwargs):
-        """Confirm that each job has either a None account_id, or that the `account_id` matches self.account_id"""
-        for identifier, job in v.items():
-            if job.account_id != values["account_id"]:
-                raise ValueError(
-                    f"The job `{identifier}` contains an `account_id` that does not match the global config account_id."
-                )
-        return v
 
     def __init__(self, **data: Any):
 
