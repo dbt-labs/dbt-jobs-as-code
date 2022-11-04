6b5e00be4f0f6c6ff133ac11db9b3ea3ae9e4ca3
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-11-04 13:17:28 +0100

    ✨ Add CRUD API calls for env var overwrite in Jobs (and allow jobs deletion)

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 import requests
 from loguru import logger
 
+from schemas.custom_environment_variable import CustomEnvironmentVariable
 from schemas.job import JobDefinition
 
 
@@ -29,6 +30,16 @@ class DBTCloud:
 
         if not self.account_id:
             raise Exception("An account_id is required to get dbt Cloud jobs.")
+
+    def build_mapping_job_identifier_job_id(self):
+        cloud_jobs = self.get_jobs()
+
+        mapping_job_identifier_job_id = {}
+        for job in cloud_jobs:
+            if job.identifier is not None:
+                mapping_job_identifier_job_id[job.identifier] = job.id
+
+        return mapping_job_identifier_job_id
 
     def update_job(self, job: JobDefinition) -> JobDefinition:
         """Update an existing dbt Cloud job using a new JobDefinition"""
@@ -70,8 +81,6 @@ class DBTCloud:
         """Delete a dbt Cloud job."""
 
         logger.debug("Deleting {job_name}. {job}", job_name=job.name, job=job)
-        logger.warning("Deletion not yet implemented.")
-        return
 
         response = requests.delete(
             url=f"{self.base_url}/api/v2/accounts/{self.account_id}/jobs/{job.id}",
@@ -101,6 +110,10 @@ class DBTCloud:
             )
 
             job_data = response.json()
+
+            if response.status_code >= 400:
+                logger.error(job_data)
+
             jobs.extend(job_data["data"])
 
             if (
@@ -129,3 +142,80 @@ class DBTCloud:
             },
         )
         return response.json()["data"]
+
+    def get_env_vars(self, project_id: int, job_id: int) -> Dict:
+        """Get the existing env vars job overwrite in dbt Cloud."""
+
+        self._check_for_creds()
+
+        response = requests.get(
+            url=(
+                f"{self.base_url}/api/v3/accounts/{self.account_id}/projects/{project_id}/environment-variables/job/?job_definition_id={job_id}"
+            ),
+            headers=self._headers,
+        )
+
+        return response.json()["data"]
+
+    def update_env_var(self, custom_env_var: CustomEnvironmentVariable, project_id: int, job_id: int) -> Dict:
+        """Update env vars job overwrite in dbt Cloud."""
+
+        self._check_for_creds()
+
+        all_env_vars = self.get_env_vars(project_id, job_id)
+
+        if custom_env_var.name not in all_env_vars:
+            raise Exception(
+                f"Custom environment variable {custom_env_var.name} not found in dbt Cloud, you need to create it first."
+            )
+
+        payload = {}
+
+        if "job" in all_env_vars[custom_env_var.name]:
+
+            if all_env_vars[custom_env_var.name]["job"]["value"] == custom_env_var.value:
+                logger.debug(f"The env var {custom_env_var.name} is already up to date for the job {job_id}.")
+                return None
+
+            payload["id"] = all_env_vars[custom_env_var.name]["job"]["id"]
+            url=(
+                    f"{self.base_url}/api/v3/accounts/{self.account_id}/projects/{project_id}/environment-variables/{payload['id']}/"
+                )
+        else:
+            payload["id"] = None
+            url=(
+                f"{self.base_url}/api/v3/accounts/{self.account_id}/projects/{project_id}/environment-variables/"
+            )
+
+        payload["account_id"] = self.account_id
+        payload["project_id"] = project_id
+        payload["type"] = "job"
+        payload["name"] = custom_env_var.name
+        payload["raw_value"] = custom_env_var.value
+        payload["job_definition_id"] = job_id
+
+        response = requests.post(
+            url=url,
+            headers=self._headers,
+            json=payload,
+        )
+
+        logger.info(f"Updated the env_var {custom_env_var.name} for job {job_id}")
+        return response.json()["data"]
+
+
+
+    def delete_env_var(self, project_id: int, env_var_id: int) -> None:
+        """Delete env_var job overwrite in dbt Cloud."""
+
+        logger.debug(f"Deleting env var id {env_var_id}")
+
+        response = requests.delete(
+            url=f"{self.base_url}/api/v3/accounts/{self.account_id}/projects/{project_id}/environment-variables/{env_var_id}/",
+            headers=self._headers,
+        )
+
+        if response.status_code >= 400:
+            logger.error(response.json())
+
+        logger.warning("Deleted successfully.")
