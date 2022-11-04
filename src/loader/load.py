779a6bc3e59afef1b80cfd6ff4890c0bd7f15072
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-11-04 13:18:18 +0100

    🐛 Fill in the identifier which was missing with `Config`

--- src/loader/load.py
+++ src/loader/load.py
@@ -7,4 +7,7 @@ def load_job_configuration(config_file) -> Config:
     """Load a job YAML file into a Config object"""
     config = yaml.safe_load(config_file)
 
+    for identifier, job in config.get("jobs").items():
+        job["identifier"] = identifier
+
     return Config(**config)
