1de58a6b82dfcc2b658c152aa3fbbaa83b1781e4
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-11-21 18:51:13 +0100

    Add logic to warn people that date/time are not required

--- src/loader/load.py
+++ src/loader/load.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 import yaml
+from loguru import logger
 
 from schemas.job import JobDefinition
 
@@ -8,6 +9,22 @@ from schemas.job import JobDefinition
 def load_job_definitions(config_file) -> Dict[str, JobDefinition]:
     """Load a job YAML file into a dictionary of JobDefinitions"""
     config = yaml.safe_load(config_file)
+
+    date_config = [
+        job.get("schedule", {}).get("date", None) for job in config["jobs"].values()
+    ]
+    time_config = [
+        job.get("schedule", {}).get("time", None) for job in config["jobs"].values()
+    ]
+
+    if any(date_config):
+        logger.warning(
+            f"❌ There is some date config under 'schedule > date' in your YML. This data is auto generated and should be deleted"
+        )
+    if any(time_config):
+        logger.warning(
+            f"❌ There is some time config under 'schedule > time' in your YML. This data is auto generated and should be deleted"
+        )
 
     return {
         identifier: JobDefinition(**job, identifier=identifier)
