7eb37c4d0d40b1f0603c6d828085dba19c46e7b2
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-12-09 17:40:08 +0100

    A function to export jobs to YML

---
+++ src/exporter/export.py
@@ -0,0 +1,16 @@
+from ruamel.yaml import YAML
+import sys
+
+from schemas.job import JobDefinition
+
+
+def export_jobs_yml(jobs: list[JobDefinition]):
+    """Export a list of job definitions to YML"""
+
+    export_yml = {"jobs": {}}
+    for id, cloud_job in enumerate(jobs):
+        export_yml["jobs"][f"import_{id + 1}"] = cloud_job.to_load_format()
+
+    yaml = YAML()
+    yaml.width = 300
+    yaml.dump(export_yml, sys.stdout)
