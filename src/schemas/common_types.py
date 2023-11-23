6f6bc9ba029ba3ae323a68ef2e2033775977d08e
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-11-23 09:33:59 +0100

    Fix for `custom_branch_only`

--- src/schemas/common_types.py
+++ src/schemas/common_types.py
@@ -11,7 +11,7 @@ class Execution(pydantic.BaseModel):
 class Triggers(pydantic.BaseModel):
     github_webhook: bool
     git_provider_webhook: Optional[bool]
-    custom_branch_only: bool
+    custom_branch_only: Optional[bool] = True
     schedule: Optional[bool]
 
 
