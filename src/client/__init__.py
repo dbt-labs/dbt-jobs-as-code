f6a7dbdde8940631d08f1022cb3d1730426a2358
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-06-05 15:52:54 +0200

    Refactor querying jobs to be more modular

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Any
 from urllib3.exceptions import InsecureRequestWarning
 
 import requests
@@ -19,7 +19,7 @@ class DBTCloud:
     def __init__(
         self,
         account_id: int,
-        api_key: str,
+        api_key: Optional[str],
         base_url: str = "https://cloud.getdbt.com",
         disable_ssl_verification: bool = False,
     ) -> None:
@@ -42,7 +42,7 @@ class DBTCloud:
                 "SSL verification is disabled. This is not recommended unless you absolutely need this config."
             )
 
-    def _clear_env_var_cache(self, job_definition_id: int) -> None:
+    def _clear_env_var_cache(self, job_definition_id: Optional[int]) -> None:
         """Clear out any cached environment variables for a given job."""
         if job_definition_id in self._environment_variable_cache:
             del self._environment_variable_cache[job_definition_id]
@@ -55,7 +55,7 @@ class DBTCloud:
         if not self.account_id:
             raise Exception("An account_id is required to get dbt Cloud jobs.")
 
-    def build_mapping_job_identifier_job_id(self, cloud_jobs: List[JobDefinition] = None):
+    def build_mapping_job_identifier_job_id(self, cloud_jobs: Optional[List[JobDefinition]] = None):
 
         if cloud_jobs is None:
             # TODO, we should filter things here at least if we call it often
@@ -87,7 +87,7 @@ class DBTCloud:
 
         return JobDefinition(**(response.json()["data"]), identifier=job.identifier)
 
-    def create_job(self, job: JobDefinition) -> JobDefinition:
+    def create_job(self, job: JobDefinition) -> Optional[JobDefinition]:
         """Create a dbt Cloud Job using a JobDefinition"""
 
         logger.debug("Creating {job_name}. {job}", job_name=job.name, job=job)
@@ -123,101 +123,84 @@ class DBTCloud:
 
         logger.success("Job deleted successfully.")
 
-    def get_jobs(self, project_id=None, environment_id=None) -> List[JobDefinition]:
+    def get_jobs(
+        self, project_ids: Optional[List[int]] = None, environment_ids: Optional[List[int]] = None
+    ) -> List[JobDefinition]:
         """Return a list of Jobs for all the dbt Cloud jobs in an environment."""
 
         self._check_for_creds()
+        project_ids = project_ids or []
+        environment_ids = environment_ids or []
 
-        offset = 0
-        list_env_ids = []
-        multiple_envs_passed = False
-        jobs = []
-
-        if project_id is None:
-            project_id = []
-        if environment_id is None:
-            environment_id = []
-
-        if len(environment_id) > 1:
-            multiple_envs_passed = True
-            list_env_ids = environment_id
-
-        # Handle the scenario, where multiple environment ID's are passed through CLI. Invoke the API once for each environment and append the jobs together
-        if multiple_envs_passed:
-
-            for env_id in list_env_ids:
-
-                while True:
-                    parameters = {"offset": offset}
-                    parameters["environment_id"] = env_id
-
-                    if len(project_id) == 1:
-                        parameters["project_id"] = project_id[0]
-                    elif len(project_id) > 1:
-                        project_id_str = [str(i) for i in project_id]
-                        parameters["project_id__in"] = f"[{','.join(project_id_str)}]"
-
-                    response = requests.get(
-                        url=f"{self.base_url}/api/v2/accounts/{self.account_id}/jobs/",
-                        params=parameters,
-                        headers=self._headers,
-                        verify=self._verify,
-                    )
-
-                    job_data = response.json()
-
-                    if response.status_code >= 400:
-                        logger.error(job_data)
-
-                    jobs.extend(job_data["data"])
-
-                    if (
-                        job_data["extra"]["filters"]["limit"]
-                        + job_data["extra"]["filters"]["offset"]
-                        >= job_data["extra"]["pagination"]["total_count"]
-                    ):
-                        break
-
-                    offset += job_data["extra"]["filters"]["limit"]
-
+        jobs: List[JobDefinition] = []
+        if len(environment_ids) > 1:
+            for env_id in environment_ids:
+                jobs.extend(self._fetch_jobs(project_ids, env_id))
+        elif len(environment_ids) == 1:
+            jobs = self._fetch_jobs(project_ids, environment_ids[0])
         else:
-            # In this case, there are no multiple environments ID's.. Invoke the API once
-            while True:
-                parameters = {"offset": offset}
-                if len(project_id) == 1:
-                    parameters["project_id"] = project_id[0]
-                elif len(project_id) > 1:
-                    project_id_str = [str(i) for i in project_id]
-                    parameters["project_id__in"] = f"[{','.join(project_id_str)}]"
-
-                if len(environment_id) == 1:
-                    parameters["environment_id"] = environment_id[0]
-
-                logger.debug(f"Request parameters {parameters}")
-                response = requests.get(
-                    url=f"{self.base_url}/api/v2/accounts/{self.account_id}/jobs/",
-                    params=parameters,
-                    headers=self._headers,
-                    verify=self._verify,
-                )
-
-                job_data = response.json()
-                if response.status_code >= 400:
-                    logger.error(job_data)
-                    return []
-
-                jobs.extend(job_data["data"])
-
-                if (
-                    job_data["extra"]["filters"]["limit"] + job_data["extra"]["filters"]["offset"]
-                    >= job_data["extra"]["pagination"]["total_count"]
-                ):
-                    break
-
-                offset += job_data["extra"]["filters"]["limit"]
+            jobs = self._fetch_jobs(project_ids, None)
 
         return [JobDefinition(**job) for job in jobs]
 
+    def _fetch_jobs(
+        self, project_ids: List[int], environment_id: Optional[int]
+    ) -> List[JobDefinition]:
+        offset = 0
+        jobs: List[JobDefinition] = []
+
+        while True:
+            parameters = self._build_parameters(project_ids, environment_id, offset)
+            job_data = self._make_request(parameters)
+
+            if not job_data:
+                return []
+
+            jobs.extend(job_data["data"])
+
+            if (
+                job_data["extra"]["filters"]["limit"] + job_data["extra"]["filters"]["offset"]
+                >= job_data["extra"]["pagination"]["total_count"]
+            ):
+                break
+
+            offset += job_data["extra"]["filters"]["limit"]
+
+        return jobs
+
+    def _build_parameters(
+        self, project_ids: List[int], environment_id: Optional[int], offset
+    ) -> dict[str, Any]:
+        parameters = {"offset": offset}
+
+        if len(project_ids) == 1:
+            parameters["project_id"] = project_ids[0]
+        elif len(project_ids) > 1:
+            project_id_str = [str(i) for i in project_ids]
+            parameters["project_id__in"] = f"[{','.join(project_id_str)}]"
+
+        if environment_id is not None:
+            parameters["environment_id"] = environment_id
+
+        logger.debug(f"Request parameters {parameters}")
+        return parameters
+
+    def _make_request(self, parameters: dict[str, Any]):
+        response = requests.get(
+            url=f"{self.base_url}/api/v2/accounts/{self.account_id}/jobs/",
+            params=parameters,
+            headers=self._headers,
+            verify=self._verify,
+        )
+
+        if response.status_code >= 400:
+            error_data = response.json()
+            logger.error(error_data)
+            return None
+
+        return response.json()
+
+    # TODO: Revisit, is it used?
     def get_job(self, job_id: int) -> JobDefinition:
         """Generate a Job based on a dbt Cloud job."""
 
@@ -290,7 +273,7 @@ class DBTCloud:
         project_id: int,
         job_id: int,
         env_var_id: int,
-        yml_job_identifier: str = None,
+        yml_job_identifier: Optional[str] = None,
     ) -> Optional[CustomEnvironmentVariablePayload]:
         """Update env vars job overwrite in dbt Cloud."""
 
