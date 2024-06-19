6052c3ce4750db61e47908599df79f5f8d6112e2
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-06-19 23:40:01 +0200

    Fix issue if there is no job returned

--- src/loader/load.py
+++ src/loader/load.py
@@ -8,16 +8,19 @@ def load_job_configuration(config_file) -> Config:
     """Load a job YAML file into a Config object"""
     config = yaml.safe_load(config_file)
 
+    if not config["jobs"]:
+        return Config(jobs={})
+
     date_config = [job.get("schedule", {}).get("date", None) for job in config["jobs"].values()]
     time_config = [job.get("schedule", {}).get("time", None) for job in config["jobs"].values()]
 
     if any(date_config):
         logger.warning(
-            f"⚡️ There is some date config under 'schedule > date' in your YML. This data is auto generated and should be deleted"
+            f"⚡️ There is some date config under 'schedule > date' in your YML. This data is auto generated and should be deleted. Only cron is supported in the config."
         )
     if any(time_config):
         logger.warning(
-            f"⚡️ There is some time config under 'schedule > time' in your YML. This data is auto generated and should be deleted"
+            f"⚡️ There is some time config under 'schedule > time' in your YML. This data is auto generated and should be deleted. Only cron is supported in the config."
         )
 
     for identifier, job in config.get("jobs").items():
