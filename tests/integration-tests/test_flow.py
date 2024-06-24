bae07a7474f78143f7c2fe79d84be1ceae73eaac
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-06-24 10:24:42 +0200

    Add test using the `-l` flag

--- tests/integration-tests/test_flow.py
+++ tests/integration-tests/test_flow.py
@@ -51,7 +51,7 @@ def clean_up_jobs():
     sys.version_info < (3, 12) or sys.version_info >= (3, 13),
     reason="We test this for Python 3.12 only to avoid issues with this test running concurrently between different versions",
 )
-def test_end_to_end_flow(runner, log_capture):
+def test_end_to_end_flow_limit_project(runner, log_capture):
     if PROJECT_ID == 0:
         raise ValueError("Please set the DBT_CLOUD_PROJECT_ID environment variable")
 
@@ -63,55 +63,123 @@ def test_end_to_end_flow(runner, log_capture):
         job_id=JOB_ID,
     )
 
+    # Create the relevant test file
     Path("tests/integration-tests/jobs_rendered.yml").write_text(rendered_job_string)
 
+    try:
+        # Call plan based on a file
+        result_plan_1 = runner.invoke(
+            cli, ["plan", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
+        )
+        assert result_plan_1.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- PLAN --" in log_output
+        assert "changes detected" in log_output
+        assert "No changes detected" not in log_output
+
+        log_capture.truncate(0)
+        log_capture.seek(0)
+
+        # Call sync based on a file
+        result_sync_1 = runner.invoke(
+            cli, ["sync", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
+        )
+        assert result_sync_1.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- SYNC --" in log_output
+        assert "| ERROR" not in log_output
+
+        log_capture.truncate(0)
+        log_capture.seek(0)
+
+        # Call plan based on a file again
+        result_plan_2 = runner.invoke(
+            cli, ["plan", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
+        )
+        assert result_plan_2.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- PLAN --" in log_output
+        assert "No changes detected" in log_output
+
+        log_capture.truncate(0)
+        log_capture.seek(0)
+
+        # Call sync based on a file again
+        result_sync_2 = runner.invoke(
+            cli, ["sync", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
+        )
+        assert result_sync_2.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- SYNC --" in log_output
+        assert "No changes detected" in log_output
+
+    finally:
+        # always clean, even on failure
+        clean_up_jobs()
+
+
+def test_end_to_end_flow_limit_yml(runner, log_capture):
+    if PROJECT_ID == 0:
+        raise ValueError("Please set the DBT_CLOUD_PROJECT_ID environment variable")
+
+    templated_job = Path("tests/integration-tests/jobs_template.yml").read_text()
+    rendered_job_string = templated_job.format(
+        project_id=PROJECT_ID,
+        account_id=ACCOUNT_ID,
+        environment_id=ENV_ID,
+        job_id=JOB_ID,
+    )
+
     # Create the relevant test file
-    # ...
+    Path("tests/integration-tests/jobs_rendered.yml").write_text(rendered_job_string)
 
-    # Call plan based on a file
-    result_plan_1 = runner.invoke(
-        cli, ["plan", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
-    )
-    assert result_plan_1.exit_code == 0
-    log_output = log_capture.getvalue()
-    assert "-- PLAN --" in log_output
-    assert "changes detected" in log_output
-    assert "No changes detected" not in log_output
+    try:
+        # Call plan based on a file
+        result_plan_1 = runner.invoke(
+            cli, ["plan", "tests/integration-tests/jobs_rendered.yml", "-l"]
+        )
+        assert result_plan_1.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- PLAN --" in log_output
+        assert "changes detected" in log_output
+        assert "No changes detected" not in log_output
 
-    log_capture.truncate(0)
-    log_capture.seek(0)
+        log_capture.truncate(0)
+        log_capture.seek(0)
 
-    # Call sync based on a file
-    result_sync_1 = runner.invoke(
-        cli, ["sync", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
-    )
-    assert result_sync_1.exit_code == 0
-    log_output = log_capture.getvalue()
-    assert "-- SYNC --" in log_output
-    assert "| ERROR" not in log_output
+        # Call sync based on a file
+        result_sync_1 = runner.invoke(
+            cli, ["sync", "tests/integration-tests/jobs_rendered.yml", "-l"]
+        )
+        assert result_sync_1.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- SYNC --" in log_output
+        assert "| ERROR" not in log_output
 
-    log_capture.truncate(0)
-    log_capture.seek(0)
+        log_capture.truncate(0)
+        log_capture.seek(0)
 
-    # Call plan based on a file again
-    result_plan_2 = runner.invoke(
-        cli, ["plan", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
-    )
-    assert result_plan_2.exit_code == 0
-    log_output = log_capture.getvalue()
-    assert "-- PLAN --" in log_output
-    assert "No changes detected" in log_output
+        # Call plan based on a file again
+        result_plan_2 = runner.invoke(
+            cli, ["plan", "tests/integration-tests/jobs_rendered.yml", "-l"]
+        )
+        assert result_plan_2.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- PLAN --" in log_output
+        assert "No changes detected" in log_output
 
-    log_capture.truncate(0)
-    log_capture.seek(0)
+        log_capture.truncate(0)
+        log_capture.seek(0)
 
-    # Call sync based on a file again
-    result_sync_2 = runner.invoke(
-        cli, ["sync", "tests/integration-tests/jobs_rendered.yml", "-p", PROJECT_ID]
-    )
-    assert result_sync_2.exit_code == 0
-    log_output = log_capture.getvalue()
-    assert "-- SYNC --" in log_output
-    assert "No changes detected" in log_output
+        # Call sync based on a file again
+        result_sync_2 = runner.invoke(
+            cli, ["sync", "tests/integration-tests/jobs_rendered.yml", "-l"]
+        )
+        assert result_sync_2.exit_code == 0
+        log_output = log_capture.getvalue()
+        assert "-- SYNC --" in log_output
+        assert "No changes detected" in log_output
 
-    clean_up_jobs()
+    finally:
+        # always clean, even on failure
+        clean_up_jobs()
