f542281331aa03ccf379f9380504890d21ac0aa3
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2025-02-06 11:55:33 +0100

    Update export code to reuse identifier when importing

--- src/dbt_jobs_as_code/exporter/export.py
+++ src/dbt_jobs_as_code/exporter/export.py
@@ -10,7 +10,8 @@ def export_jobs_yml(jobs: list[JobDefinition], include_linked_id: bool = False):
 
     export_yml = {"jobs": {}}
     for id, cloud_job in enumerate(jobs):
-        export_yml["jobs"][f"import_{id + 1}"] = cloud_job.to_load_format(include_linked_id)
+        yaml_key = cloud_job.identifier if cloud_job.identifier else f"import_{id + 1}"
+        export_yml["jobs"][yaml_key] = cloud_job.to_load_format(include_linked_id)
 
     print(
         "# yaml-language-server: $schema=https://raw.githubusercontent.com/dbt-labs/dbt-jobs-as-code/main/src/dbt_jobs_as_code/schemas/load_job_schema.json"
